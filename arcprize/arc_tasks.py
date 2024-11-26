9e59a4a19178dc19ccd4c9902b69ec97071fb78c
Richard Lack <rilack@microsoft.com>
2024-11-26 12:17:29 -0800

    update prompts

--- arcprize/arc_tasks.py
+++ arcprize/arc_tasks.py
@@ -5,6 +5,14 @@ from arcprize.case_renderer import case_pair_text, case_pair_list_text, row_grou
 def get_case(context: TaskContext) -> ArcCase:
     return ArcCase(**context.session.app_context['case'])
 
+#*TODO: add system prompt step
+# class ArcSystemPrompt(SystemPromptStep):
+#     """You are an expert puzzle solver and an expert software engineer,
+#     in a quiet environment where you can think carefully, focus and think to do an exam.
+#     """
+#     role = DialogRole.SYSTEM # This line won't actually be needed because of how we subclassed
+
+
 
 class IntroduceProblem(DocstringPromptStep):
     """We are working on a problem from the ARC Prize, and you are a smart and
@@ -14,26 +22,37 @@ class IntroduceProblem(DocstringPromptStep):
 
     Let's look at the input and output pairs from the training set for this particular problem.
     There is some kind of transformation rule that goes from the input items to the the output items.
-    Let's look at all the items from the training set, which are problems where they give us
-    the answers:
+    Let's look at all the items from the training set where they gave us pairs of inputs with their answers.
    
     {train_set_str}
 
-    Let's look at the first case.  This is the first "Test" item, the one that we need to solve.
-    This is a Test case like an Exam, one that we must think very hard about.
-    Given this input, consider what you think the output should be.
+    Take some time to look at the patterns here and consider what rule or pattern is being used
+    to get from the inputs to the outputs.  You can think of a 0 as being blank or black, usually.
+    The other values you can think of as corresponding to different colors, and the full matrix
+    creating an image.  Often various kinds of patterns emerge in doing the transformation to
+    the output.
+    """
+    def prompt_variables(self, context: TaskContext) -> dict:
+        case = get_case(context)
+        train_set_str = case_pair_list_text(case.train, 'Training')
+        return {'train_set_str': train_set_str}
+
+
+class ProposeSolution1(DocstringPromptStep):
+    """Now let's move on to the first "Test" item, the first one that we need to think through
+    and solve ourselves. This is a Test case like an Exam, one that we must think very hard about.
+    We can also think of it as a Test in the sense that all the above cases are Training cases.
+    Given this input, consider what you think the output should be:
 
     {input_item_str}
     """
-    # [ ] show all the train questions
-    # [ ] show the input side of the test question
-    def prompt_variables(self, context: TaskContext):
+    def prompt_variables(self, context: TaskContext) -> dict:
         case = get_case(context)
-        train_set_str = case_pair_list_text(case.train, 'Training')
         input_item_str = row_group_text(case.test[0].input, 0, 'Input', 'Test')
-        return {'train_set_str': train_set_str,
-                'input_item_str': input_item_str,
-                'case_id': context.session.app_context['case_id'] # temp
-                }
+        return {'input_item_str': input_item_str}
 
-all_arc_task_classes = [IntroduceProblem]
+
+all_arc_task_classes = [
+    IntroduceProblem,
+    ProposeSolution1,
+]
